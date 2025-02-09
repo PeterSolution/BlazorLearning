13dca8fe9e3b2112e3eebacaef5d2eb0ddfd3107
PeterSolution <piotrwolff@op.pl>
2025-02-09 17:39:53 +0100

    blazor learning upload

---
+++ BlazorLearn/Models/DataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorLearn.Models
+{
+    public class DataModel
+    {
+        public int IdData { get; set; }
+        public string CreateUser { get; set; }
+        public string Title { get; set; }
+        public string Date { get; set; }
+    }
+}
---
+++ BlazorLearn/Program.cs
@@ -0,0 +1,45 @@
+using BlazorLearn.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+
+builder.Services.AddScoped(
+    newscope => new HttpClient { 
+        BaseAddress = new Uri("https://localhost:7116/") });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
